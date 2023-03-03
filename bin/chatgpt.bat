6ccb854aae19c35caecf8b55b99623484187a49e
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2023-03-03 00:42:45 -0500

    created chat gpt helper

---
+++ bin/chatgpt.bat
@@ -0,0 +1,26 @@
+@REM Runs the gpt complete chat command
+@echo off
+
+@REM Get the current directory
+set currentDir=%cd%
+
+@REM Get the current drive
+set currentDrive=%cd:~0,1%
+
+@REM Ensure you are on correct drive
+D:
+
+@REM Navigate to root for windows
+cd /
+
+@REM TODO: Navigate to project root
+cd {INPUT DIRECTORY}
+
+@REM Run the command
+node .\lib\scripts\chat.js %*
+
+@REM Navigate back to original drive
+%currentDrive%:
+
+@REM Navigate back to original directory
+cd %currentDir%
